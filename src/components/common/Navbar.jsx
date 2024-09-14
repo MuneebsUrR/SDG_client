764763081f54d97b4242b4ee170c94ff71ee4ae3
Muneeb Ur Rehman <muneebsurrehman@gmail.com>
2024-09-14 20:02:35 +0500

    updated navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -4,6 +4,7 @@ import { ThemeSwitcher } from "./themeSwitcher";
 import { Menu } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useSidebarStore } from "./useSidebarStore";
+import PersonIcon from '@mui/icons-material/Person';
 import logo from '../../assets/logo.png';
 
 const Navbar = () => {
@@ -26,24 +27,26 @@ const Navbar = () => {
   return (
     <nav className={`fixed top-0 left-0 w-full z-30 transition-colors duration-300 ${scrolled ? 'bg-primary' : 'bg-transparent'}`}>
       <div className="bg-transparent flex flex-row justify-between items-center gap-5 px-4 lg:pb-4">
-        <a href="/" className="mt-1 transform transition-transform duration-150">
-          <img src={logo} alt="" width={70} height={70} />
-        </a>
-
-        <h4 className="text-white font-bold text-xl opacity-100 uppercase leading-6 hidden lg:block transition-opacity duration-150">
-          BSG for SDG
-        </h4>
 
         <div className="text-white flex flex-row gap-3 lg:gap-5 transition-transform duration-150">
-          <ThemeSwitcher />
+          <a href="/" className="mt-2 transform transition-transform duration-150">
+            <img src={logo} alt="" width={70} height={70} />
+          </a>
+          <div className="flex items-center justify-center">
           <Button
             size="lg"
-            className="bg-transparent text-white"
+            className="bg-transparent text-white "
             isIconOnly
             onClick={toggleSideNavbar}
           >
             <Menu size={40} />
           </Button>
+          </div>
+        </div>
+
+        <div className="text-white flex flex-row gap-2 transition-transform duration-150">
+          <ThemeSwitcher />
+           <PersonIcon fontSize="large" />
         </div>
       </div>
     </nav>
